9d17e9418eb2029b9d34aecce10407f2631b87d0
ajenggdn <diajengnoviana2127@gmail.com>
2024-03-06 21:54:13 +0700

    first commit

---
+++ resources/views/home.blade.php
@@ -0,0 +1,7 @@
+@extends('layout.app')
+
+@section('content')
+
+<h1>halaman profile</h1>
+
+@endsection
---
+++ resources/views/profile.blade.php
@@ -0,0 +1,7 @@
+@extends('layout.app')
+
+@section('content')
+
+<h1>Halaman home</h1>
+
+@endsection
